a031610a82dabdde33fb96a3f7340d503289b926
chaim <chaimyisroel@gmail.com>
2021-08-17 18:12:51 -0400

    commit tree e45eb7a8da5aac1784dde4191e3327e696991e0b

--- main.js
+++ main.js
@@ -3,7 +3,48 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+///
+const list = document.querySelectorAll('article')
 
+for (const item of list) {
+  const heart = item.querySelector('.like-glyph')
+  //console.log(heart)
+  
+  heart.addEventListener('click', (e) => {
+    if (heart.innerText === EMPTY_HEART) {
+    //console.log('event clicked!')
+    mimicServerCall()
+    .then(data => {
+      //console.log('succees')
+      // changing the heart to red
+      heart.classList.add('activated-heart')
+      // changing to a fuul heart
+      heart.innerText = FULL_HEART
+    })
+    .catch(error => {
+      console.log('error')
+      // showing error banner
+      const modal = document.getElementById('modal');
+      //console.log(modal)
+      modal.classList.remove('hidden')
+      // adding error message to red banner
+      const modalMessage = document.getElementById('modal-message')
+      modalMessage.innerText = error
+      // remove error banner afrer 3 seconds
+      setTimeout(() => {
+        modal.classList.add('hidden')
+      }, 3000)
+    })
+
+  } else if (heart.innerText === FULL_HEART){
+      //remove red heart
+      heart.classList.remove('activated-heart')
+      // changing to a empty heart
+      heart.innerText = EMPTY_HEART
+  }
+})
+
+}
 
 
 
